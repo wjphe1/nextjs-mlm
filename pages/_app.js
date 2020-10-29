381f8c6760ca26f24efc37b303e70738aeae0f58
wjphe1 <lawliet5145@gmail.com>
2020-10-29 15:39:27 +0800

    update

---
+++ pages/_app.js
@@ -0,0 +1,24 @@
+import '../styles/main.scss'
+import React from 'react'
+import App from 'next/app'
+
+export default class MyApp extends App {
+    static async getInitialProps({ Component, router, ctx }) {
+        let pageProps = {}
+        if (Component.getInitialProps) { pageProps = await Component.getInitialProps(ctx) }
+        return { pageProps }
+    }
+
+    state = {
+        // global states here
+        name: "Wei Jun",
+    }
+
+    render () {
+        const { Component, pageProps } = this.props
+
+        return (
+            <Component {...pageProps} {...this.state}/>
+        )
+    }
+}
--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,14 +1,20 @@
+import Head from 'next/head'
 import Link from 'next/link'
+import Layout from '../../components/layout'
+
 
 export default function FirstPost() {
   return (
-    <>
-      <h1>First Post 12</h1>
-      <h2>
-        <Link href="/">
-          <a>Back to home</a>
-        </Link>
-      </h2>
-    </>
+    <Layout>
+        <Head>
+            <title>First Post</title>
+        </Head>
+        <h1>First Post 12</h1>
+        <h2>
+            <Link href="/">
+            <a>Back to home</a>
+            </Link>
+        </h2>
+    </Layout>
   )
 }
