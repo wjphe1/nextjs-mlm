caccd326b37dffa0820b43e79f3dbd069d33e4e9
Phee Wei Jun <junz@Phees-MacBook-Air.local>
2021-10-14 17:39:14 +0800

    changed api url

--- components/auth/api.js
+++ components/auth/api.js
@@ -1,9 +1,9 @@
 import Axios from "axios";
 
 let urls = {
-    test: `http://13.212.45.145/api/v1/`,
-    development: 'http://13.212.45.145/api/v1/',
-    production: 'https://reezqa-api.cerealsproject.com/api/v1/'
+    test: `https://reezqa.staging.griter.io/api/v1/`,
+    development: 'https://reezqa.staging.griter.io/api/v1/',
+    production: 'https://reezqa.staging.griter.io/api/v1/'
 }
 const api = Axios.create({
     baseURL: urls[process.env.NODE_ENV],
