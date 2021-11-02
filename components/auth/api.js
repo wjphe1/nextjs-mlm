98a1f0beada67af87d16dd553c51b814de751aae
Phee Wei Jun <lawliet5145@gmail.com>
2021-11-02 13:15:10 +0800

    changed api url

--- components/auth/api.js
+++ components/auth/api.js
@@ -1,9 +1,9 @@
 import Axios from "axios";
 
 let urls = {
-    test: `https://reezqa.staging.griter.io/api/v1/`,
-    development: 'https://reezqa.staging.griter.io/api/v1/',
-    production: 'https://reezqa.staging.griter.io/api/v1/'
+    test: `https://reeqza.staging.griter.io/api/v1/`,
+    development: 'https://reeqza.staging.griter.io/api/v1/',
+    production: 'https://reeqza.staging.griter.io/api/v1/'
 }
 const api = Axios.create({
     baseURL: urls[process.env.NODE_ENV],
